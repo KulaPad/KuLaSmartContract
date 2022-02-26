fc9ecc6518d557de35e6bb2d5f0add4ae74cbaee
Tran Thanh Phong <phong.tran@grove-dean.co.uk>
2022-02-26 21:52:40 +0700

    Remove unused package

--- token-factory/build.sh
+++ token-factory/build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cargo build --target wasm32-unknown-unknown --release
 cp target/wasm32-unknown-unknown/release/token_factory.wasm token_factory.wasm 
+
+read -p "Press any key to continue..."
