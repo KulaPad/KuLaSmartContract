be7a6b813f83b5e018279dc65719aff906a82f41
neo.mxn0 <neo.mxn0@gmail.com>
2022-03-10 04:02:25 +0700

    ADD TODO

--- staking-pool/src/enumeration.rs
+++ staking-pool/src/enumeration.rs
@@ -25,17 +25,19 @@ impl StakingContract {
     }
 
     pub fn get_account_info(&self, account_id: AccountId) -> AccountJson {
+        // TODO: Luat: .unwrap() is dangerous and need to be handle
+        // TODO: Handle the case user have not staked any token
         let upgradable_account: UpgradableAccount = self.accounts.get(&account_id).unwrap();
         let account: Account = Account::from(upgradable_account);
         let new_reward = self.internal_calculate_account_reward(&account);
 
-        AccountJson { 
-            account_id: account_id, 
+        AccountJson {
+            account_id: account_id,
             lock_balance: U128(account.lock_balance),
             unlock_timestamp: account.unlock_timestamp,
-            stake_balance: U128(account.stake_balance), 
-            unstake_balance: U128(account.unstake_balance), 
-            reward: U128(account.pre_reward + new_reward), 
+            stake_balance: U128(account.stake_balance),
+            unstake_balance: U128(account.unstake_balance),
+            reward: U128(account.pre_reward + new_reward),
             can_withdraw: account.unstake_available_epoch_height <= env::epoch_height(),
             start_unstake_timestamp: account.unstake_start_timestamp,
             unstake_available_epoch: account.unstake_available_epoch_height,
@@ -44,10 +46,10 @@ impl StakingContract {
     }
 
     pub fn get_pool_info(&self) -> PoolInfo {
-        PoolInfo { 
-            total_stake_balance: U128(self.total_stake_balance), 
-            total_reward: U128(self.pre_reward + self.internal_calculate_global_reward()), 
-            total_stakers: U128(self.total_staker), 
+        PoolInfo {
+            total_stake_balance: U128(self.total_stake_balance),
+            total_reward: U128(self.pre_reward + self.internal_calculate_global_reward()),
+            total_stakers: U128(self.total_staker),
             is_paused: self.paused
         }
     }
